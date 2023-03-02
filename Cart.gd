cbc4731014a73990c2c0385492af017b1d318944
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-03-01 19:47:37 -0500

    add all files

---
+++ Cart.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+signal hit
+
+export var speed = 0.25
+var screen_size = Vector2.ZERO
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	print("velocity", velocity) 
+	print("speed", speed)
+	print("delta", delta)
+	
+	position += velocity * speed * delta
+	position.x = clamp(position.x,0,screen_size.x)
+	position.y = clamp(position.y,0,screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right_left"
+		$AnimatedSprite.flip_h = velocity.x > 0
+		$AnimatedSprite.flip_v = false
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+		$AnimatedSprite.flip_h = false
+
+func start(new_position):
+	position = new_position
+	show()
